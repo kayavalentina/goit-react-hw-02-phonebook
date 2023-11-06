a5c875103a3017a79c3193602370b8d4aaa910ff
kayavalentina <kayavalentina1993@gmail.com>
2023-11-06 19:45:50 +0100

    task1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+//import Notiflix from 'notiflix';
+
+
+import  phonebook  from './data/phonebook.json';
+import  AddContact  from './AddContact/AddContact';
+import  ContactList  from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [...phonebook],
+    name: ''
+  };
+
+  handleAddContact = (name) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+    };
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <AddContact onAddContact={this.handleAddContact} />
+        <ContactList contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
+
+export default App;
